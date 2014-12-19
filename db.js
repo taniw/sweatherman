1d683e1c35cc66f265241aa6b3aefa7b594e786e
Tani Wong <tani.s.wong@gmail.com>
2014-12-18 17:12:32 -0800

    Prepare db

--- db.js
+++ db.js
@@ -4,15 +4,17 @@ var pg = require('pg');
 var db = {};
 
 db.config = {
-  database: "weatherdb",
-  port: 5432,
-  host: "localhost"
+  // database: "weatherdb",
+  // port: 5432,
+  // host: "localhost"
 };
 
 db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err) {
-      console.error("something is wrong", err);
+      console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
     runAfterConnecting(client);
     done();
